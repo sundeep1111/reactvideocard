b20c0b8e4a9876dbb3aa76a10b7e0bba79c04700
sundeep <sundeep1111-code@gmail.com>
2020-08-30 07:46:18 +0530

    Updates

---
+++ precache-manifest.447733c2c67a33ea74dbf4127e9b653e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc20d8846b0b836a46b4503ac38c061c",
+    "url": "/finaltodolist/index.html"
+  },
+  {
+    "revision": "a8b959d4fb2d6821663e",
+    "url": "/finaltodolist/static/css/main.e17771c0.chunk.css"
+  },
+  {
+    "revision": "324bed551f70a74c465c",
+    "url": "/finaltodolist/static/js/2.35614203.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/finaltodolist/static/js/2.35614203.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b959d4fb2d6821663e",
+    "url": "/finaltodolist/static/js/main.77abf236.chunk.js"
+  },
+  {
+    "revision": "ed2fa598ca984e64c915",
+    "url": "/finaltodolist/static/js/runtime-main.4c7924cc.js"
+  }
+]);
